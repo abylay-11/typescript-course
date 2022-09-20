be4e0db109182ce7673234c404d46199a0f12920
Abylay Madalkhan <abylay.madalkhan@gmail.com>
2022-09-20 17:03:54 +0600

    Generics

--- lesson-4/index.ts
+++ lesson-4/index.ts
@@ -1,5 +1,7 @@
 // Generics
 
+import axios from "axios";
+
 interface ValueContainer<Value> {
   value: Value;
 }
@@ -33,6 +35,21 @@ class ArrayOfAnything<T> {
     return this.collection[index];
   }
 }
-
+``
 new ArrayOfAnything<string>(["1", "2", "3"]);
 new ArrayOfAnything<number>([1, 2, 3, 4, 5]);
+
+function map<Type>(arr: Type[], cb: (item: Type) => Type): Type[] {
+  return arr.map(cb)
+}
+const arr = ['string', 'stinng2']
+
+const arr2 = map<string>(arr, (item) => {
+  return item
+})
+
+interface user {
+  name: string
+}
+
+const response = axios.post("registration")
